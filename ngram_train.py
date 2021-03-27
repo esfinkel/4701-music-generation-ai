74fcce61e4adf9600ca62ba2bc06e173b0258296
cel243 <cel243@cornell.edu>
2021-03-27 18:39:34 -0400

    counts collected

--- ngram_train.py
+++ ngram_train.py
@@ -2,6 +2,9 @@ import os
 from collections import defaultdict
 
 def gather_counts(directory):
+  """Finds unigram, bigram, and trigram counts for all **kern files in 
+  `directory`. prepends two <s> lines to the beginning of every file
+  and appends two </s> lines to every file. """
   counts_un = defaultdict(int)
   counts_bi = defaultdict(int)
   counts_tri = defaultdict(int)
@@ -23,6 +26,21 @@ def gather_counts(directory):
       counts_bi[prev+"\n"+"</s>\n"] += 1
       counts_tri[prev_prev+"\n"+prev+"\n" + "</s>\n"] += 1
       counts_tri[prev+"\n</s>\n</s>\n"] += 1
+  return counts_un, counts_bi, counts_tri
+
+# def ln_probs(counts, l1, l2, l3):
+#   """Creates dictionary of log probabilities, where:
+#   -  the probability of line1 line2 line3 is lambda1 * P(line3 | line1 line2) + 
+#   lambda2 * P(line3 | line2) + lambda3 * P(line3)
+#   - the probability of line1 line2 is lambda"""
+
+if __name__ == "__main__":
+  counts_un, counts_bi, counts_tri = gather_counts("music_in_C")
+  un_counts = [(line, count) for line, count in counts_un.items()]
+  un_counts.sort(key=lambda x: x[1], reverse=True)
+  for line, count in un_counts[:10]:
+    print(line)
+    print(count)
       
 
         
