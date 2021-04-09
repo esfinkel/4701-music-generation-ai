a0c4391ecbc02ce9076301350a9fcdc9e7459c43
cel243 <cel243@cornell.edu>
2021-04-09 11:14:04 -0400

    convert vectors back into kern

--- kern_to_vec2.py
+++ kern_to_vec2.py
@@ -14,12 +14,13 @@ representation.
 # AAAA to ccccc - the 88 piano keys
 note = 'AAAA'
 note_to_ind_map = {}
-duration_to_ind_map = {}
 for i in range(88):
     note_to_ind_map[note] = i*3
     note = transpose_pieces.transpose_note_standalone(note, 1)
-## 88 * 3 = 264, then 265 and 266 are taken, so rest occurs 267-269
-note_to_ind_map['r'] = 267
+## 88 * 3 = 264
+note_to_ind_map['r'] = 264
+
+ind_to_note_map = {ind : note for note, ind in note_to_ind_map.items()}
 
 def zeros(hands=2):
     return np.zeros(270*hands, dtype=np.int16) 
@@ -29,35 +30,20 @@ def get_vec_for_hand(hand_notes):
     vec = zeros(hands=1)
     notes = hand_notes.strip().split(" ")
     for i in range(len(notes)):
-        print(notes[i])
         if notes[i].strip() == ".":
             continue
         dur = music_helpers.convert_to_dur_frac(notes[i])
         dur_n, dur_d = dur.numerator, dur.denominator
-        print(dur_n, dur_d)
         note = music_helpers.get_note_note(notes[i])
-        print(note)
         if note not in note_to_ind_map:
             # note doesn't exist on piano because of upward transposition; 
             # so transpose down
             note = note[1:]
-        note_ind = note_to_ind_map.get(note)
+        note_ind = note_to_ind_map[note]
         vec[note_ind] = 1 # if appears twice somehow, is still 1
         vec[note_ind + 1] = dur_n 
         vec[note_ind + 2] = dur_d
     return vec
-
-def test():
-    # With current logic, the RNN will be able to differentiate between the
-    # respective durations of the different 3 notes in the hand. idk if
-    # that's necessary
-    res1 = get_vec_for_hand("4r 8aa 16ee") 
-    for i in range(len(res1)):
-        if res1[i] != 0:
-            print(i, res1[i])
-    # I'm too lazy to work out what this actually should be, but 
-    # looks plausible - 2 notes found
-# test()
 
 def convert_kern_line_to_vec(line):
     """ Bag of notes, where each note position is 1 if the note is present
@@ -86,17 +72,40 @@ def convert_kern_line_to_vec(line):
 
     return np.concatenate((get_vec_for_hand(l_notes), get_vec_for_hand(r_notes)))
 
-def convert_vec_to_kern(line_vec):
-    """The inverse of convert_kern_line_to_vec()"""
-    pass
+def convert_hand_vec_to_kern(hand_vec):
+    hand_notes = []
+    for i in range(0,len(hand_vec),3):
+        if hand_vec[i] != 0:
+            note = ind_to_note_map[i]
+            duration = float(hand_vec[i+1]) /  hand_vec[i+2]
+            duration_notated = music_helpers.convert_duration_to_notation(duration)
+            hand_notes.append(duration_notated + note)
+    hand_notes.sort(key=lambda x: music_helpers.convert_to_duration(x))
+    return " ".join(hand_notes)
 
-
+def convert_line_vec_to_kern(line_vec):
+    """The inverse of convert_kern_line_to_vec(), possibly with ordering
+    changes of the notes. """
+    left_notes = convert_hand_vec_to_kern(line_vec[:len(zeros(hands=1))])
+    right_notes = convert_hand_vec_to_kern(line_vec[len(zeros(hands=1)):])
+    return left_notes+"\t"+right_notes
+    
 def vec_list_for_song(lines):
-    # should add zero vec to start/end of song, as mentioned in
-    # `convert_kern_line_to_vec`
     vec_list = [zeros()]
     for line in lines:
-        # TODO maybe normalize before appending
         vec_list.append(convert_kern_line_to_vec(line))
     vec_list.append(zeros())
     return vec_list
+
+def test():
+    vec = convert_kern_line_to_vec("4C 4d\t16r 2.aa 4ddddd")
+    print(convert_line_vec_to_kern(vec))
+    # res1 = get_vec_for_hand("32r 8ee 16aa") 
+    # for i in range(len(res1)):
+    #     if res1[i] != 0:
+    #         print(i, res1[i])
+    # print(convert_hand_vec_to_kern(res1))
+
+if __name__ == "__main__":
+    test()
+    # print(ind_to_note_map)
