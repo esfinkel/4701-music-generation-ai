3227152352416ddc54d24db78a6a8e70fcf2d073
cel243 <cel243@cornell.edu>
2021-04-15 14:15:50 -0400

    sampling over beat vocabulary

---
+++ vocab_vec_generate.py
@@ -0,0 +1,81 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import init
+import torch.optim as optim
+import math
+import random
+import os
+import io
+import pickle
+
+import time
+from tqdm import tqdm
+
+import vocab_vectors as kern2vec
+from rnn_vocab import RNN_No_FFNN
+import fix_kern
+
+
+def write_music(formatted):
+    """Given well-formatted kern music, write file"""
+    dir = 'generated_music_RNN'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    with open(f"./{dir}/{round(time.time())}.txt", "w") as file:
+        file.write(formatted)
+
+def generate_from_piece(model, file_lines):
+    file_vecs = kern2vec.vec_list_for_song(file_lines)
+    hidden = model.init_hidden()
+    song_string = ""
+    for vec in file_vecs:
+        line, hidden = model(vec, hidden)
+        line_str = kern2vec.convert_line_vec_to_kern(line.detach().numpy())
+        # line_str = kern2vec.convert_line_vec_to_kern(vec)
+        if line_str is not None:
+            song_string += line_str + "\n"
+    print("Converting music to good **kern....")
+    new_music_formatted = fix_kern.convert_to_good_kern(song_string)
+    print("Writing music.....")
+    write_music(new_music_formatted)
+
+def generate(model, max_lines=150):
+    """
+    Generate and write random **kern song of length "max_lines",
+    using the RNN "model".
+
+    The param `updated_hidden_format` is True.
+    """
+    print("generating music.....")
+    curr_line = torch.from_numpy(kern2vec.ones()).float() 
+    hidden = model.init_hidden()
+    song_string = ""
+    i=0
+    while i <= max_lines:
+        i+= 1
+        try:
+            curr_line = curr_line.detach().numpy()
+        except:
+            pass
+        curr_line, hidden = model(curr_line, hidden)
+        line_str = kern2vec.convert_line_vec_to_kern(curr_line.detach().numpy())
+        if line_str is not None:
+            song_string += line_str + "\n"
+            curr_line = kern2vec.convert_kern_line_to_vec(line_str)
+    print("Converting music to good **kern....")
+    new_music_formatted = fix_kern.convert_to_good_kern(song_string)
+    print("Writing music.....")
+    write_music(new_music_formatted)
+
+
+if __name__ == "__main__":
+    model = None
+    with open('rnn_models/vocab_vecs&hidden_dim=100&learning_rate=0.25&epoch=4&dist=45.621517181396484', "rb") as f:
+        model = torch.load(f)
+    generate(model)
+    # with open("./music_in_C/Beethoven, Ludwig van___Piano Sonata no. 8 in C minor") as f:
+    #     file_lines = f.readlines()
+    # generate_from_piece(model, file_lines)
+
+
---
+++ vocab_vectors.py
@@ -0,0 +1,120 @@
+import numpy as np
+import re
+import music_helpers
+import math
+import random
+import torch
+import fix_kern
+import os
+
+import transpose_pieces
+
+def get_next_beat(lines, i):
+    """returns a concatenation of the lines after i that form
+    a multiple of 1 quarter note."""
+    next_line = ""
+    r_dur = 0
+    l_dur = 0
+    j = i
+    for line in lines[i:]:
+        j += 1
+        if line.strip() == "":
+            continue
+        if len(list(filter(lambda x : x.strip() != "", line.split("\t")))) < 2:
+            continue
+        l_notes, r_notes = line.split("\t")[0], line.split("\t")[1]
+        r_dur += music_helpers.convert_to_duration(music_helpers.get_duration_of_spine(r_notes))
+        l_dur += music_helpers.convert_to_duration(music_helpers.get_duration_of_spine(l_notes))
+        next_line += (line.strip()+"\n").replace("[","").replace("]","")
+        if r_dur%0.25<=0.01 and l_dur%0.25<=0.01:
+            break
+        if r_dur > 2 or l_dur > 2:
+            return get_next_beat(lines, i+1)
+    return next_line, j
+    
+
+def gather_vocab():
+    """ Gather all beat occurrences in the training data.  """
+    vocab = set()
+    for filename in os.listdir("./music_in_C_training"):
+        if ".DS_Store" in filename:
+            continue
+        with open(f"./music_in_C_training/{filename}", "r") as f:
+            filetext = f.readlines()
+            filetext = list(filter(lambda t: t.strip() != "", filetext))
+            i = 0
+            while i < len(filetext):
+                next_line, i = get_next_beat(filetext, i)
+                vocab.add(next_line)
+    for filename in os.listdir("./music_in_C_test"):
+        if ".DS_Store" in filename:
+            continue
+        with open(f"./music_in_C_test/{filename}", "r") as f:
+            filetext = f.readlines()
+            filetext = list(filter(lambda t: t.strip() != "", filetext))
+            i = 0
+            while i < len(filetext):
+                next_line, i = get_next_beat(filetext, i)
+                vocab.add(next_line)
+    return vocab
+
+vocab = gather_vocab()
+vocab_to_ind = {token : i for i, token in enumerate(list(vocab))}
+ind_to_vocab = {i : token for token, i in vocab_to_ind.items()}
+
+
+def zeros():
+    return np.zeros(len(ind_to_vocab), dtype=np.int16) 
+
+def ones():
+    return np.ones(len(ind_to_vocab), dtype=np.int16) 
+
+def to_good_prob_dist(vec):
+    ## inverse of softmax is log(S_i) + c
+    ## since our vectors are already in log space, we just need
+    ## to shift probabilities to 0
+    return vec - np.min(vec)
+
+def convert_line_vec_to_kern(line_vec):
+    """The inverse of convert_kern_line_to_vec(), possibly with ordering
+    changes of the notes. """
+    ind = random.choices(range(len(line_vec)), to_good_prob_dist(line_vec))[0]
+    return ind_to_vocab[ind]
+
+def convert_kern_line_to_vec(line):
+    vec = np.zeros(len(ind_to_vocab))
+    vec[vocab_to_ind[line]] = 1
+    return vec
+  
+def vec_list_for_song(lines):
+    """Converts a list of kern lines into a list of vectors. """
+    vec_list = [ones()]
+    lines = list(filter(lambda t: t.strip() != "", lines))
+    i = 0
+    while i < len(lines):
+        next_line, i = get_next_beat(lines, i)
+        vec = np.zeros(len(ind_to_vocab))
+        vec[vocab_to_ind[next_line]] = 1
+        vec_list.append(vec)
+    vec_list.append(ones())
+    return vec_list
+
+def song_from_vec_list(vecs):
+    """Converts a list of vectors into a string representing a kern song."""
+    song = ""
+    for vec in vecs:
+        song_kern = convert_line_vec_to_kern(vec)
+        song += song_kern
+    return song        
+
+def test():
+    with open("./music_in_C_training/Chopin, Frederic___Mazurka in C-sharp Minor, Op. 63, No. 3") as f:
+        song = f.readlines()
+    vecs = vec_list_for_song(song)
+    converted_song = song_from_vec_list(vecs)
+    with open("./test_song", "w") as f:
+        f.write(converted_song)
+
+if __name__ == "__main__":
+    test()
+    # print(ind_to_note_map)
